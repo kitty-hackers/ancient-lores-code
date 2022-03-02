180703c540f2020edddda3f43f5c046c64f2568f
Paul Petrov <paul@bargamut.ru>
2022-03-02 14:09:26 +0300

    Remove all CRA & make it more primitive: install Babel & presets

--- src/components/LinksList/LinksList.tsx
+++ src/components/LinksList/LinksList.tsx
@@ -1,7 +1,7 @@
 import { VFC } from "react";
-import classNames from "classnames";
+import classnames from "classnames/bind";
 import { Link } from "../Link";
-import "./LinksList.css";
+import styles from "./LinksList.less";
 
 type TListEntry = { caption: string; key: string; to: string };
 
@@ -9,18 +9,21 @@ type TProps = {
 	data: TListEntry[];
 };
 
+const cx = classnames.bind(styles);
+const className = "links-list";
+
 export const LinksList: VFC<TProps> = (props) => {
 	const { data } = props;
 
 	return (
-		<ul className="links-list">
+		<ul className={cx(className)}>
 			{data.map((item) => {
 				const { key, ...linkProps } = item;
 
-				const className = classNames(["link-list__item", `link-list__item--${key.toLowerCase()}`]);
+				const classNames = cx([`${className}__item`, `${className}__item--${key.toLowerCase()}`]);
 
 				return (
-					<li className={className} key={key}>
+					<li className={classNames} key={key}>
 						<Link {...linkProps} />
 					</li>
 				);
