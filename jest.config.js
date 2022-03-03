fc48430455d94205f47173f8952d780abc50502a
Paul Petrov <paul@bargamut.ru>
2022-03-03 19:20:15 +0300

    Install Jest & React Testing Library

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
 	transformIgnorePatterns: [
 		"/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime).+(js|jsx|ts|tsx)$",
 	],
+	moduleNameMapper: {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+		"\\.(css|less)$": "identity-obj-proxy"
+	}
 };
