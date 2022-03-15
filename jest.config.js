51f7413ebd540aec6a1151a0f8f9c6eb4d44c905
Paul Petrov <paul@bargamut.ru>
2022-03-15 19:51:00 +0300

    Fix Jest config via fileMock & styleMock

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+	moduleNameMapper: {
+		"\\.(css|less)$": "identity-obj-proxy",
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+			"<rootDir>/__mocks__/fileMock.ts",
+	},
 	testEnvironment: "jsdom",
 	transformIgnorePatterns: [
 		"/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime).+(js|jsx|ts|tsx)$",
 	],
-	moduleNameMapper: {
-		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-		"\\.(css|less)$": "identity-obj-proxy"
-	}
 };
